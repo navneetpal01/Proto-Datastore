38328c2b21b090dfbf8869751953884607044d99
navneet <navneetpalgithub@gmail.com>
2024-06-26 16:31:11 +0530

    Marss

--- app/src/main/java/com/example/proto_datastore/MainActivity.kt
+++ app/src/main/java/com/example/proto_datastore/MainActivity.kt
@@ -7,20 +7,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.datastore.dataStore
 import com.example.proto_datastore.ui.theme.ProtoDatastoreTheme
 import kotlinx.collections.immutable.mutate
-
+import kotlinx.coroutines.launch
 
 
 // So in datastore whenever the preferences changes our states are automatically updated and keeps our UI up-to-date
 
-val Context.dataStore by dataStore("app-settings.json",AppSettingsSerializer)
+val Context.dataStore by dataStore("app-settings.json", AppSettingsSerializer)
+
+val appSettings = AppSettings().toString()
 
 class MainActivity : ComponentActivity() {
 
@@ -40,24 +50,43 @@ class MainActivity : ComponentActivity() {
                 val scope = rememberCoroutineScope()
                 Column(
                     modifier = Modifier
-                        .fillMaxSize()
-                ){
-
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    for (i in 0..2) {
+                        val language = Language.values()[i]
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            RadioButton(
+                                selected = language == appSettings.language,
+                                onClick = {
+                                    scope.launch {
+                                        setLanguage(language = language)
+                                    }
+                                }
+                            )
+                            Spacer(modifier = Modifier.width(8.dp))
+                            Text(text = language.toString())
+                        }
+                    }
                 }
             }
         }
 
     }
-    private suspend fun setLanguage(language: Language){
+
+    private suspend fun setLanguage(language: Language) {
         dataStore.updateData {
             it.copy(
                 language = language,
                 knownLocations = it.knownLocations.mutate {
                     it.add(
                         Location(
-                        15.0,
-                        15.0
-                    )
+                            15.0,
+                            15.0
+                        )
                     )
                 }
             )
