d8e95b1c8443a95dfe0fca30f8684f74f5b2d6b1
navneet <navneetpalgithub@gmail.com>
2024-06-26 13:37:54 +0530

    Marss

--- app/src/main/java/com/example/proto_datastore/AppSettingsSerializer.kt
+++ app/src/main/java/com/example/proto_datastore/AppSettingsSerializer.kt
@@ -1,20 +1,34 @@
 package com.example.proto_datastore
 
 import androidx.datastore.core.Serializer
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.Json
 import java.io.InputStream
 import java.io.OutputStream
 
 
-object AppSettingsSerializer : Serializer<AppSettings>{
+//Point - A input stream always returns bytes
+
+object AppSettingsSerializer : Serializer<AppSettings> {
     override val defaultValue: AppSettings
-        get() = TODO("Not yet implemented")
+        get() = AppSettings()
 
     override suspend fun readFrom(input: InputStream): AppSettings {
-        TODO("Not yet implemented")
+        return try {
+            Json.decodeFromString(
+                deserializer = AppSettings.serializer(),  // plugin generated this function
+                string = input.readBytes().decodeToString()
+            )
+        }catch (e : SerializationException){
+            e.printStackTrace()
+            defaultValue
+        }
     }
 
     override suspend fun writeTo(t: AppSettings, output: OutputStream) {
-        TODO("Not yet implemented")
+        output.write(
+
+        )
     }
 
 
